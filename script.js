37a8f19bf869c51679b755d8317c34f29ce0858f
Akshay <152252912+akhssm@users.noreply.github.com>
2025-11-03 11:13:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 function getFormvalue() {
     //Write your code here
+	event.preventDefault(); 
 
+  const firstName = document.forms["form1"]["fname"].value.trim();
+  const lastName = document.forms["form1"]["lname"].value.trim();
+
+  if (!firstName && !lastName) {
+    alert("Please enter your first and last name.");
+    return;
+  }
+
+  alert(`${firstName} ${lastName}`);
 }
